d1d03e470040f187bcf79abef3160121237973a2
Sergey Batin <the.batin@gmail.com>
2025-03-28 20:43:45 -0700

    debug plugins configuration rework

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,13 +1,13 @@
 return {
   {
     'mfussenegger/nvim-dap',
-    dependencies = 'jedrzejboczar/nvim-dap-cortex-debug',
     init = function()
       vim.fn.sign_define('DapBreakpoint',          { text = '', texthl = 'debugBreak', linehl = '' })
       vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = 'debugBreak', linehl = '' })
       vim.fn.sign_define('DapBreakpointRejected',  { text = '', texthl = 'debugBreak', linehl = '' })
       vim.fn.sign_define('DapStopped',             { text = '󰌕', texthl = 'debugStopped', linehl = 'debugPC' })
-
+    end,
+    config = function()
       local dap = require('dap')
 
       dap.adapters['arm-gdb'] = {
@@ -20,20 +20,24 @@ return {
         type = 'executable',
         command = '/opt/homebrew/opt/llvm/bin/lldb-vscode',
       }
-
-      -- see https://github.com/jedrzejboczar/nvim-dap-cortex-debug for more info
-      require('dap-cortex-debug').setup({
-        debug = false,       -- log debug messages
-        -- path to cortex-debug extension, supports vim.fn.glob
-        -- by default tries to guess: mason.nvim or VSCode extensions
-        extension_path = nil,
-        lib_extension = nil, -- shared libraries extension, tries auto-detecting, e.g. 'so' on unix
-        node_path = 'node',  -- path to node.js executable
-        dapui_rtt = true,    -- register nvim-dap-ui RTT element
-        -- make :DapLoadLaunchJSON register cortex-debug for C/C++, set false to disable
-        dap_vscode_filetypes = { 'c', 'cpp' },
-      })
     end
+  },
+
+  {
+    'jedrzejboczar/nvim-dap-cortex-debug',
+    dependencies = 'mfussenegger/nvim-dap',
+    -- see https://github.com/jedrzejboczar/nvim-dap-cortex-debug for more info
+    opts = {
+      debug = false,       -- log debug messages
+      -- path to cortex-debug extension, supports vim.fn.glob
+      -- by default tries to guess: mason.nvim or VSCode extensions
+      extension_path = nil,
+      lib_extension = nil, -- shared libraries extension, tries auto-detecting, e.g. 'so' on unix
+      node_path = 'node',  -- path to node.js executable
+      dapui_rtt = true,    -- register nvim-dap-ui RTT element
+      -- make :DapLoadLaunchJSON register cortex-debug for C/C++, set false to disable
+      dap_vscode_filetypes = { 'c', 'cpp' },
+    }
   },
 
   {
